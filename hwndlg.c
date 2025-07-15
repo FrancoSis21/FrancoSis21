10b8e27c5a77030d342de0c69eceebd22f8b7629
franquito gg <133679484+FrancoSis21@users.noreply.github.com>
2025-07-14 21:43:40 -0500

    Create hwndlg.c
    my first code from this rep

---
+++ hwndlg.c
@@ -0,0 +1,17 @@
+#include <stdlib.h> 
+
+
+int main(){
+
+  case WM_PAINT:
+{
+PAINTSTRUCT ps; 
+  HDC hdc = beginPait(hwndlg, &ps);
+
+  
+  endPain(hwndlg, &ps); 
+};
+return TRUE; 
+
+return 0; 
+}; 
